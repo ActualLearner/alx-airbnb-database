889c3a746b096f99f70335d2dbdb0df2c2110c55
Abdellah <abdellahshafi7@gmail.com>
2025-07-05 08:29:04 +0300

    Update and rename performance.sql to perfomance.sql

--- database-adv-script/perfomance.sql
+++ database-adv-script/perfomance.sql
@@ -15,16 +15,7 @@ JOIN booking b ON p.booking_id = b.booking_id
 JOIN property pr ON b.property_id = pr.property_id
 JOIN users u ON u.user_id = b.user_id
 
-/* Result: 1	SIMPLE	p		ALL	    booking_id				4	100.00	
-           1	SIMPLE	b		eq_ref	PRIMARY,property_id,user_id	PRIMARY	144	airbnb.p.booking_id	1	100.00	
-           1	SIMPLE	pr	eq_ref	PRIMARY	PRIMARY	144	airbnb.b.property_id	1	100.00	
-           1	SIMPLE	u		eq_ref	PRIMARY	PRIMARY	144	airbnb.b.user_id	1	100.00	
-We can observe that a full table scan takes place when querying for booking_id inside the p(payment) table.
-
-*/
-
 -- Improvements:
-
 
 -- 1. Add index on payment(booking_id) to avoid a full table scan
 CREATE INDEX idx_payment_booking_id ON payment(booking_id);
@@ -53,14 +44,3 @@ FROM payment p
 JOIN booking b ON p.booking_id = b.booking_id
 JOIN property pr ON b.property_id = pr.property_id
 JOIN users u ON u.user_id = b.user_id
-
-/* 1	SIMPLE	p		ALL	idx_payment_booking_id				4	100.00	
-   1	SIMPLE	b		eq_ref	PRIMARY,property_id,user_id	PRIMARY	144	airbnb.p.booking_id	1	100.00	
-   1	SIMPLE	pr		eq_ref	PRIMARY	PRIMARY	144	airbnb.b.property_id	1	100.00	
-   1	SIMPLE	u		eq_ref	PRIMARY	PRIMARY	144	airbnb.b.user_id	1	100.00	
-
-Our index is not being used, most probably due to our payment table being of small size(which makes the compiler
-choose a full table scan over an index since it's cheaper. However we keep this index since it will become 
-crucial when our dataset grows.
-
-*/
