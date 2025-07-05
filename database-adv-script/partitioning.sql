488ebd99bb1c1dd02b539e4e7ffeb748b2355fc0
Abdellah <abdellahshafi7@gmail.com>
2025-07-05 08:59:37 +0300

    Create partitioning.sql

---
+++ database-adv-script/partitioning.sql
@@ -0,0 +1,18 @@
+CREATE TABLE booking (
+  booking_id CHAR(36) PRIMARY KEY,
+  property_id CHAR(36) NOT NULL,
+  user_id CHAR(36) NOT NULL,
+  start_date DATE NOT NULL,
+  end_date DATE NOT NULL,
+  total_price DECIMAL(10,2) NOT NULL,
+  status ENUM('pending', 'confirmed', 'canceled') NOT NULL,
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  FOREIGN KEY (property_id) REFERENCES property(property_id),
+  FOREIGN KEY (user_id) REFERENCES users(user_id)
+) PARTITION BY RANGE YEAR(start_date)(
+    PARTITION p2019 VALUES LESS THAN (2019),
+    PARTITION p2020 VALUES LESS THAN (2020),
+    PARTITION p2021 VALUES LESS THAN (2021),
+    PARTITION p2022 VALUES LESS THAN (2022),
+    PARTITION pMax VALUES LESS THAN MAXVALUE
+)
